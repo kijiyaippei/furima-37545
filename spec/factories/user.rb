8ba1adf075005ac8c22960c0f02b0d4cd555f574
kijiyaippei <kijiya0722@gmail.com>
2022-07-11 08:57:35 +0900

    ユーザ管理2

---
+++ spec/factories/user.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+    factory :user do
+    email {Faker::Internet.free_email}
+    #password = Faker::Internet.password(min_length: 6)
+    password {'aaa111'}
+    password_confirmation {password}
+    nickname {Faker::Name}
+    last_name {'山田'}
+    first_name {'太郎'}
+    last_name_kana {'ヤマダ'}
+    first_name_kana {'タロウ'}
+    birth_date {Faker::Date.backward}
+    
+    end
+    end
+    
